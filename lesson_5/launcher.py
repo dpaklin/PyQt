a592e37890b92dcbeba5f5678d1e50b3fd7986b6
dragly <72442659+dpaklin@users.noreply.github.com>
2022-01-13 12:22:18 +0300

    lesson_5

---
+++ lesson_5/launcher.py
@@ -0,0 +1,22 @@
+"""Программа-лаунчер"""
+
+import subprocess
+
+process = []
+
+while True:
+    action = input('Выберите действие: q - выход , s - запустить сервер, c - запустить клиенты x - закрыть все окна:')
+    if action == 'q':
+        break
+    elif action == 's':
+        # Запускаем сервер!
+        process.append(subprocess.Popen('python server.py', shell=True, encoding='utf8'))
+    elif action == 'c':
+        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
+        # Запускаем клиентов:
+        for i in range(clients_count):
+            process.append(
+                subprocess.Popen(f'python client.py -n test{i + 1}', shell=True, encoding='utf8'))
+    elif action == 'x':
+        while process:
+            process.pop().kill()
